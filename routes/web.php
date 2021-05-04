fa00e4459cfee895016c16beb86caa80d29fffa0
Nikolay <nikolaynigorodov@gmail.com>
2021-05-04 13:22:56 +0300

    middleware for controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [PagesController::class, 'test'])->middleware('auth');
 
 Auth::routes();
 
